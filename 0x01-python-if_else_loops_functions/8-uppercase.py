207018bd3d316650d2231c775a4a661632cb5662
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-05-10 15:45:04 +0200

    task 8 fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(s):
     output = ""
     for i in range(len(s)):
         if ord(s[i]) >= 97 and ord(s[i]) <= 122:
-            output += chr(ord(s[i]) + (97 - 65))
+            output += chr(ord(s[i]) - (97 - 65))
         else:
             output += s[i]
     print(output)
