3b213670d37225688f8878c84fff0ee6573c017b
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-05-10 16:37:49 +0200

    task 11 fix

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,6 +2,10 @@
 
 def pow(a, b):
     output = a
-    for i in range(2, b+1):
-        output *= a
+    if b >= 0:
+        for i in range(2, b+1):
+            output *= a
+    else:
+        for i in range(1, (-1 * b)+2):
+            output /= a
     return output
