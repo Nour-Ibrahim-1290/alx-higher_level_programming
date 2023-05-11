07b74f5a519eebb4df69f1b83cb173a15eca147a
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-05-11 18:57:36 +0200

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    num_args = len(args)
+    ending = "s" if num_args != 1 else ""
+
+    print("{} argument{}".format(num_args, ending), end="")
+    if num_args == 0:
+        print(".")
+    else:
+        print(":")
+        for i, arg in enumerate(args):
+            print("{}: {}".format(i+1, arg))
