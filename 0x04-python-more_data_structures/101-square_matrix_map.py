78ea7c15ab81f6fe47fa1e514aeacba57f0f6b1d
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-05-19 23:47:46 +0200

    0x04:task 14-fix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_map(matrix=[]):
-    def square(y):
-        return list(map(lambda x: x ** 2, y))
-    return list(map(square, matrix))
+    return list(map(lambda y: list(map(lambda x: x ** 2, y)), matrix))
