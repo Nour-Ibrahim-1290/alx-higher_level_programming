f9690a0c881f36f1436a7e5dc64cef7fa5955dc5
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-05-19 19:14:01 +0200

    0x04:update Test

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dict):
-    if a_dict is None:
+    if not a_dict:
         return None
 
-    max_val = max(a_dict.values())
-    for key in a_dict.keys():
-        if a_dict[key] == max_val:
-            return key
+    return max(a_dict, key = a_dict.get)
