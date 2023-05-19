7084a04ed797402a30261d23a963f260d1a04ab2
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-05-20 00:17:21 +0200

    0x04:task 13-update

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    if my_list == []:
+    if not my_list:
         return 0
-    weighted_sum = sum([score[0] * score[1] for score in my_list])
-    weights = sum([score[1] for score in my_list])
-    return weighted_sum / weights
+
+    sum_w = sum(map(lambda pair: pair[0] * pair[1], my_list))
+    return sum_w / sum(map(lambda pair: pair[1], my_list))
