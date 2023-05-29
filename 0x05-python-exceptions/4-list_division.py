222abf4345eb8f7f0c36ed76400e0a2e7d153040
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-05-29 19:38:15 +0200

    0x05-Py task04

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+
+    """
+    INPUT: my_list_1, my_list_2, list_length
+    OUTPUT: list = division of the 2 lists with
+        length = Min(length of 1st list, len of 2nd list, list_length)
+    """
+    result = []
+    try:
+        for i in range(list_length):
+            try:
+                result.append(my_list_1[i] / my_list_2[i])
+            except TypeError:
+                print("wrong type")
+                result.append(0)
+                continue
+            except ZeroDivisionError:
+                print("division by 0")
+                result.append(0)
+                continue
+    except IndexError:
+        print("out of range")
+    finally:
+        return result
