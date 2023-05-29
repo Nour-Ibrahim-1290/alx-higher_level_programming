528f25c9129ae1e1068c62cb611d5517e585b140
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-05-29 19:00:49 +0200

    0x05-Py task03 fix

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,7 +7,7 @@ def safe_print_division(a, b):
         result = a/b
     except ZeroDivisionError:
         result = None
-
-    print("Inside result: {}".format(result))
+    finally:
+        print("Inside result: {}".format(result))
 
     return result
