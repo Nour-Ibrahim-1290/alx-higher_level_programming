beb175367ef9dce41dbc06641bd730d44acde400
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-05-30 19:36:29 +0200

    0x06-Py task00 fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
+"""Define a Square Class"""
+
+
 class Square:
+    """Represent a Square"""
 
     def __init__(self):
+        """
+        Args:
+            self: used by Python to initiate Square instances
+        """
         pass
