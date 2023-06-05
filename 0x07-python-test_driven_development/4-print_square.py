1e83c35dbb07af62c661e16541d18e9653ce3070
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-06-05 11:38:26 +0200

    0x07-Py task03

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,24 @@
+#!/bin/usr/python3
+"""This Module holds a function that prints squares"""
+
+
+def print_square(size):
+    """Prints A Square with size using '#' character
+    Raises:
+        TypeError: if size is not an integer or
+                    if it was both less than 0 and float
+        ValueError: if size == 0
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        if isinstance(size, float):
+            raise TypeError("size must be an integer")
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
