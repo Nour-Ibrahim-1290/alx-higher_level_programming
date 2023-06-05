d8c69318f5d2558a8492aa66f43bf6a1b7ab0591
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-06-05 08:58:03 +0200

    0x07-Py task01

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,10 +5,11 @@ All numbers are casted into integers before summation.
 
 Raises: TypeError: in case that any of the arguments is non-number."""
 
+
 def add_integer(a, b=98):
     """Return the addition of two numbers a and b.
-
-    Chacks type of argument and raises TypeError in case of non-number arguments."""
+    Chacks type of argument and raises TypeError
+    in case of non-number arguments."""
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This Module contains a function that divides a matrix to a number"""
+
+
+def matrix_divided(matrix, div):
+    """Return a new matrix which is the result of dividing matrix by div"""
+
+    # checks for div (non-number and 0 checks)
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    # Check that all items in matrix are lists
+    if sum([not isinstance(item, list) for item in matrix]) != 0:
+        msg = "matrix must be a matrix (list of lists) of integers/floats"
+        raise TypeError(msg)
+
+    # check for length of every row
+    row_len = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_len:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    # check for values inside each row
+    for row in matrix:
+        if sum([not isinstance(value, (int, float)) for value in row]) != 0:
+            raise TypeError(msg)
+
+    # Perform Divion operation (matrix / div)
+    result = []
+    for row in matrix:
+        new_row = []
+        for value in row:
+            new_row.append(round(value / div, 2))
+        result.append(new_row)
+
+    return result
