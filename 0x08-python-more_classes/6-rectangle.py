073d2b36d65d10fe48470060dccb3bdecd0bc227
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-06-06 09:32:18 +0200

    0x08-Py task06

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represents a rectangle."""
+
+    __width = 0
+    __height = 0
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """
+        Define a new Object instance of Rectangle
+        Args:
+            width (int): The width of the new rectangle object.
+            height (int): The height of the new rectangle object.
+        """
+        self.width = width
+        self.height = height
+        type(self).number_of_instances += 1
+
+    @property
+    def width(self):
+        """Get/set the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        """Calculate the area of the rectangle instance"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Calculate the perimeter of the rectangle instance"""
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Represent the object as a string."""
+        if self.width == 0 or self.height == 0:
+            return ""
+        else:
+            return '\n'.join('#' * self.width for i in range(self.height))
+
+    def __repr__(self):
+        """Represent the object as a printable string."""
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
+        """Message in case of deleting an instance."""
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
