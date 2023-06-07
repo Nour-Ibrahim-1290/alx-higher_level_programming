f565728849eabfc586afe9467c5397a7b114c4d3
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-06-07 02:20:03 +0200

    0x07-Py task05,06

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This Module Holds a Function to indent text"""
+
+
+def text_indentation(text):
+    """Prints text after replacing ., ?, and : with \n\n
+    Raises: TypeError if text is not a string
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
