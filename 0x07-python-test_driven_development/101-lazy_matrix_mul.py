636d531873791f593385f5528ccea7ec7beaea5c
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-09-07 21:07:22 +0200

    0x07-Adv. #2

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import numpy as np
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    Input: 2 matrices to multiply
+    Output: the result mutliplied matrix if valid
+    """
+    return np.matmul(m_a, m_b)
