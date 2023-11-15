21f9ce31ea6925e7080432296bc17d12c5821c65
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-11-14 23:42:14 -0800

    craete table task #4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- create table to the current database on MYSQL server
+-- the database name will be passed as an argument
+CREATE TABLE IF NOT EXISTS first_table(
+	id INT,
+	name VARCHAR(256)
+);
