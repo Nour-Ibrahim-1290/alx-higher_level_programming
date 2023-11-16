24e3c9b6ab4170dd9af9c4fd7a0c545bc7432761
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-11-15 18:49:02 -0800

    task #10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- list all shows contained in hbtn_0d_tvshows taht have al least one genra.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
