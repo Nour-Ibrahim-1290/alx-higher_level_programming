f678765ab91bd28da15c3c6f5d866473d2707100
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-12-11 04:47:59 -0800

    JS-T#10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// print factorial
+
+const num = parseInt(process.argv[2]);
+
+function factorial (num) {
+  if (isNaN(num) || num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+console.log(factorial(num));
