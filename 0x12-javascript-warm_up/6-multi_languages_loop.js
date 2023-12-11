0714a308d2b0b6545aae09dacefb6a36ae33efaf
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-12-11 03:25:00 -0800

    JS-T#6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// print 3 lines using array and loop
+
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
