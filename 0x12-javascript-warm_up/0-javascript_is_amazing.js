61ff13c54ae06a1d80ee550b97f3eae566ffc124
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-12-11 03:06:35 -0800

    JS-T#4

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,6 @@
 #!/bin/usr/node
 
-//print a message
+// print a message
 
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+// print two arguments passes to it with "is" in between
+
+console.log(process.argv[2] + ' is ' + process.argv[3]);
