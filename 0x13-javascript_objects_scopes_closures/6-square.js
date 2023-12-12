32ef9a696f635f73e973a22186bea72cefcbe75e
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-12-12 14:53:09 +0200

    JS2-T#6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// define a Square subclass with exta instance functions
+
+const Rectangle = require('./4-rectangle.js');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
